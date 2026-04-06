f899f6bf1ce76910968f4fea98d9e6dc70600faa
MichaelEnny <moe0100@mcdaniel.edu>
2026-04-05 19:23:21 -0700

    Fix legacy digest migration for fresh databases

--- db/migrations/002_legacy_digest_compat.sql
+++ db/migrations/002_legacy_digest_compat.sql
@@ -1,7 +1,20 @@
 alter table if exists digests
   add column if not exists generated_title text;
 
-update digests
-set generated_title = title
-where generated_title is null
-  and title is not null;
+do $$
+begin
+  if exists (
+    select 1
+    from information_schema.columns
+    where table_schema = 'public'
+      and table_name = 'digests'
+      and column_name = 'title'
+  ) then
+    execute '
+      update digests
+      set generated_title = title
+      where generated_title is null
+        and title is not null
+    ';
+  end if;
+end $$;
